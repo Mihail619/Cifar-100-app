e6de3cc80611512bd6891509333b074f69a09bc1
mihail <mehaz@yandex.ru>
2024-05-04 16:28:07 +0300

    Создано приложение. Организовано взаимодействие между модулями. Изображения загружаются и выполняется предсказание. Качество модели все еще плохое.

--- image_model/model.py
+++ image_model/model.py
@@ -1,13 +1,20 @@
 import cv2
-import io
+import os
+import random
 import numpy as np
 import datetime
 import onnxruntime
+import json
 
 
 '''Реализуется предсказание модели с помощью библиотеки onnxruntime и OpenCV'''
 
-def transform_image(image_bytes):
+def get_possible_classes():
+    with open('image_model\classes.json') as file:
+        data = json.load(file)
+    return data
+
+def transform_image(image):
     '''Функция преобразует изображение в нужный формат, размер и нормализует'''
 
     mean = np.array([0.50707516, 0.48654887, 0.44091784])
@@ -15,7 +22,8 @@ def transform_image(image_bytes):
 
     # Откроем изображение
     # image = Image.open(io.BytesIO(image_bytes))
-    image = cv2.imread(image_bytes)
+    # image = cv2.imread(image_bytes)
+    image = cv2.imdecode(np.frombuffer(image, np.uint8), cv2.IMREAD_COLOR)
     
     # Приведем изображение к нужному формату
     image = cv2.resize(image, (32, 32))
@@ -29,11 +37,14 @@ def transform_image(image_bytes):
     # приведем массив в формат np.float32
     img_normalized = img_normalized.astype(np.float32)
 
-    return img_normalized
+    # Добавляем размерность для мини-батча
+    image = np.expand_dims(img_normalized, axis=0)
+
+    return image
 
 
 
-def get_image_class_prediction(image_bytes):
+def get_image_class_prediction(image):
     '''
     Ф-я выполняет предсказание класса изображения файлу изображения
     '''
@@ -43,8 +54,7 @@ def get_image_class_prediction(image_bytes):
     output_name = session.get_outputs()[0].name
 
     # преобразуем изображение  
-    img = transform_image(image_bytes=image_bytes,
-                          )
+    img = transform_image(image=image)
     
     # выполняем предсказание моделью
     output = session.run([output_name], {input_name: img})
@@ -52,8 +62,53 @@ def get_image_class_prediction(image_bytes):
     # получим вероятность предсказания каждого из символов
     result_probability = output[0][0]
 
+    # получим индекс символа который был предсказан с наибольшей вероятностью
+    predicted_index = int(result_probability.argmax())
+    
+    # получим словарь классов изображений и их меток
+    classes_dict = get_possible_classes()
+
+    # получим название символа который был предсказан с наибольшей вероятностью
+    predicted_class = classes_dict[str(predicted_index) ]
+    
+    return predicted_class
+
+
+def get_result(image_file, is_api=False) -> dict:
+
+    # зафиксируем начальное время
+    start_time = datetime.datetime.now()
+
+    print(type(image_file))
+
+    # выполним предсказание моделью и получим класс изображения (название символа)
+    class_name = get_image_class_prediction(image=image_file)
+    # Проверка успешности загрузки изображения
+    if image_file is not None:
+        print("Изображение успешно загружено.")
+    else:
+        print("Ошибка при загрузке изображения.")
+    # зафиксируем время окончания предсказания
+    end_time = datetime.datetime.now()
+
+    # вычислим сколько времени затрачено на предсказание в милисекундах
+    time_diff = (end_time - start_time)
+    execution_time = f'{round(time_diff.total_seconds()*1000)} ms'
+
+    return {
+        'class_name': class_name,
+        'execution_time': execution_time
+    }
+
+
 
 if __name__ == '__main__':
-    with open('static\images\M.png', 'rb') as f:
-        result = get_image_class_prediction(f.read())
-    # print(result)
+    dir_list = os.listdir("tests\\images")
+    file_name = random.choice(dir_list)
+    with open(f'tests\\images\\{file_name}', 'rb') as f:
+        result = get_result(f.read())
+
+    print(result)
+    
+    cv2.imshow(file_name, cv2.imread(f'tests\\images\\{file_name}'))
+    cv2.waitKey(0)
--- src/main.py
+++ src/main.py
@@ -1,13 +1,24 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Form, File, UploadFile
+import numpy as np
+
+from image_model import model
 
 app = FastAPI()
 
 
-@app.get("/image_class_prediction")
-async def image_class_prediction():
-    return 
-    ...
+@app.post("/image_class_prediction")
+async def image_class_prediction(file: UploadFile = File(...)):
+    '''
+    Ф-я выполняет предсказание класса изображения файлу изображения
+    '''
+    contents = await file.read()
+    np_img = np.fromstring(contents, np.uint8)
+    
+    image_class = model.get_result(np_img)
+    return {"class is": image_class}
+    
 
-@app.get("/image_class_prediction_api/")
+@app.post("/image_class_prediction_api/")
 async def image_class_prediction():
     ...
